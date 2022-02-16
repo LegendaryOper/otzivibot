e119a6ff6de5438fc1eeab1a1407efafe19a9323
LegendaryOper <artemgris66@gmail.com>
2022-02-16 20:42:20 +0300

    bebra

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 host = 'eu-cdbr-west-02.cleardb.net'
-user = 'bc3154fa92f83f'
-password = 'd913b9a5'
-db_name = 'heroku_9fbed21acd1139c'
+user = 'bfaed1f63d14f9'
+password = 'f0aa8a81'
+db_name = 'heroku_6afd88ab725b4dd'
